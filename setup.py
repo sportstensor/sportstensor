53fa97ba2506a4e98962b555492f32b46b58d979
conidig <conidig@gmail.com>
2024-10-03 08:57:33 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -63,13 +63,13 @@ with codecs.open(
     version_string = version_match.group(1)
 
 setup(
-    name="bittensor_subnet_template",  # TODO(developer): Change this value to your module subnet name.
+    name="sportstensor",  # TODO(developer): Change this value to your module subnet name.
     version=version_string,
-    description="bittensor_subnet_template",  # TODO(developer): Change this value to your module subnet description.
+    description="sportstensor",  # TODO(developer): Change this value to your module subnet description.
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/opentensor/bittensor-subnet-template",  # TODO(developer): Change this url to your module subnet github url.
-    author="bittensor.com",  # TODO(developer): Change this value to your module subnet author name.
+    url="https://github.com/xzistance/sportstensor",  # TODO(developer): Change this url to your module subnet github url.
+    author="sportstensor.com",  # TODO(developer): Change this value to your module subnet author name.
     packages=find_packages(),
     include_package_data=True,
     author_email="",  # TODO(developer): Change this value to your module subnet author email.
