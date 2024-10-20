803f18ae50e2cdeede9e4564ed78d6efc422fd06
Xzistance <165348230+xzistance@users.noreply.github.com>
2024-10-20 08:29:16 -0500

    Nba base (#76)
    * adding base model for miners for NBA
    
    * fixing import. adding nba to test script and printing our incentive v2 probability predictions

---
+++ st/models/basketball_nba.py
@@ -0,0 +1,16 @@
+import random
+import bittensor as bt
+from st.sport_prediction_model import SportPredictionModel
+from common.data import ProbabilityChoice
+
+from st.models.basketball import BasketballPredictionModel
+
+
+class NBABasketballPredictionModel(BasketballPredictionModel):
+    def make_prediction(self):
+        bt.logging.info("Predicting NBA basketball game...")
+        
+        # Set your probability predictions here
+        #self.prediction.probabilityChoice = random.choice([ProbabilityChoice.HOMETEAM, ProbabilityChoice.AWAYTEAM])
+        #self.prediction.probability = 0.5
+        
