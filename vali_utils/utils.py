e37d41d3a4db7d8853127dcfb8b5ada2d7f76338
Xzistance <xzistance3@gmail.com>
2024-12-02 16:25:34 -0600

    new minimum probability constant. invalidating predictions that are below or equal to our threshold.

--- vali_utils/utils.py
+++ vali_utils/utils.py
@@ -21,7 +21,8 @@ from common.constants import (
     IS_DEV,
     VALIDATOR_TIMEOUT,
     SCORING_CUTOFF_IN_DAYS,
-    LEAGUES_ALLOWING_DRAWS
+    LEAGUES_ALLOWING_DRAWS,
+    MIN_PROBABILITY
 )
 
 from neurons.validator import Validator
@@ -659,10 +660,10 @@ def is_match_prediction_valid(
             f"Probability {prediction.probability} is not a float",
         )
     
-    if round(prediction.probability, 4) <= 0 or round(prediction.probability, 4) > 1:
+    if round(prediction.probability, 4) <= MIN_PROBABILITY or round(prediction.probability, 4) > 1:
         return (
             False,
-            f"Probability {prediction.probability} rounded to {round(prediction.probability, 4)} is not between 0 and 1",
+            f"Probability {prediction.probability} rounded to {round(prediction.probability, 4)} is not between {MIN_PROBABILITY} and 1",
         )
     
     # Check that the current time is before the match date
