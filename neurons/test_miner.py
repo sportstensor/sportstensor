2404fd257138774a3182c7483ba901a87c6c097e
Xzistance <xzistance3@gmail.com>
2024-12-18 11:07:07 -0600

    Adding in Sportstensor base model for connecting to TheOddsAPI and providing a prediction.

--- neurons/test_miner.py
+++ neurons/test_miner.py
@@ -1,10 +1,10 @@
 import datetime as dt
-import os
 from common.data import Sport, League, MatchPrediction
 from st.sport_prediction_model import make_match_prediction
+import bittensor as bt
 
-
-# from sportstensor.predictions import make_match_prediction
+bt.logging.set_trace(True)
+bt.logging.set_debug(True)
 
 
 def mls():
@@ -20,9 +20,9 @@ def mls():
 
     match_prediction = make_match_prediction(match_prediction)
 
-    print(
+    bt.logging.info(
         f"Match Prediction for {match_prediction.awayTeamName} at {match_prediction.homeTeamName} on {matchDate}: \
-    Prediction: {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with probability {match_prediction.probability}"
+        {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with wp {round(match_prediction.probability, 4)}"
     )
 
     return match_prediction
@@ -41,69 +41,65 @@ def mlb():
 
     match_prediction = make_match_prediction(match_prediction)
 
-    print("match_prediction", match_prediction)
-
-    print(
+    bt.logging.info(
         f"Match Prediction for {match_prediction.awayTeamName} at {match_prediction.homeTeamName} on {matchDate}: \
-    Prediction: {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with probability {match_prediction.probability}"
+        {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with wp {round(match_prediction.probability, 4)}"
     )
 
 def epl():
-    matchDate = "2024-09-20"
+    matchDate = "2024-12-20"
     match_prediction = MatchPrediction(
-        matchId=1234,
+        matchId="0b25bc4bd29ca0cd5d4b8031a3a36480",
         matchDate=dt.datetime.strptime(matchDate, "%Y-%m-%d"),
         sport=Sport.SOCCER,
         league=League.EPL,
-        homeTeamName="Arsenal",
-        awayTeamName="Chelsea",
+        homeTeamName="Aston Villa",
+        awayTeamName="Manchester City",
     )
 
     match_prediction = make_match_prediction(match_prediction)
 
-    print("match_prediction", match_prediction)
-
-    print(
+    bt.logging.info(
         f"Match Prediction for {match_prediction.awayTeamName} at {match_prediction.homeTeamName} on {matchDate}: \
-    Prediction: {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with probability {match_prediction.probability}"
+        {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with wp {round(match_prediction.probability, 4)}"
     )
 
 def nfl():
-    matchDate = "2024-09-20"
+    matchDate = "2024-12-20"
     match_prediction = MatchPrediction(
-        matchId=1234,
+        matchId="d308633813328ef6c47859652c6970e2",
         matchDate=dt.datetime.strptime(matchDate, "%Y-%m-%d"),
         sport=Sport.FOOTBALL,
         league=League.NFL,
-        homeTeamName="Cincinnati Bengals",
-        awayTeamName="Tampa Bay Buccaneers",
+        homeTeamName="Los Angeles Chargers",
+        awayTeamName="Denver Broncos",
     )
 
     match_prediction = make_match_prediction(match_prediction)
 
-    print(
+    bt.logging.info(
         f"Match Prediction for {match_prediction.awayTeamName} at {match_prediction.homeTeamName} on {matchDate}: \
-    Prediction: {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with probability {match_prediction.probability}"
+        {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with wp {round(match_prediction.probability, 4)}"
     )
 
     return match_prediction
 
 def nba():
-    matchDate = "2024-09-20"
+    matchDate = "2024-12-20"
     match_prediction = MatchPrediction(
-        matchId=1234,
+        matchId="ad71b94e2848f96ac01305e357df1e8a",
         matchDate=dt.datetime.strptime(matchDate, "%Y-%m-%d"),
         sport=Sport.BASKETBALL,
         league=League.NBA,
-        homeTeamName="Los Angeles Lakers",
-        awayTeamName="Boston Celtics",
+        homeTeamName="Detroit Pistons",
+        awayTeamName="Utah Jazz",
     )
 
     match_prediction = make_match_prediction(match_prediction)
 
-    print(
+    bt.logging.info(
         f"Match Prediction for {match_prediction.awayTeamName} at {match_prediction.homeTeamName} on {matchDate}: \
-    Prediction: {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with probability {match_prediction.probability}"
+        {match_prediction.probabilityChoice} ({match_prediction.get_predicted_team()}) with wp {round(match_prediction.probability, 4)}"
     )
 
     return match_prediction
@@ -112,5 +108,5 @@ if __name__ == "__main__":
     #mls()
     #mlb()
     #epl()
-    #nfl()
-    nba()
+    nfl()
+    #nba()
