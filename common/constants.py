cbe760988c55179eefb8fbf9f584639f6f10c657
Xzistance <165348230+xzistance@users.noreply.github.com>
2025-09-03 13:23:54 -0500

    Enabling NFL for 2025 season (#182)
    * Enabling NFL for 2025 season
    
    * bumping to 5% starting weight

--- common/constants.py
+++ common/constants.py
@@ -78,7 +78,8 @@ ROI_SCORING_WEIGHT = 0.5
 ACTIVE_LEAGUES = [
     League.MLS,
     League.MLB,
-    League.EPL
+    League.EPL,
+    League.NFL
 ]
 
 LEAGUES_ALLOWING_DRAWS = [
@@ -96,11 +97,11 @@ ROLLING_PREDICTION_THRESHOLD_BY_LEAGUE = {
 
 # MUST ADD UP to 1.0 (100%)
 LEAGUE_SCORING_PERCENTAGES = {
-    League.MLB: 0.87,
+    League.MLB: 0.82,
     League.NBA: 0.0,
     League.EPL: 0.03,
     League.MLS: 0.10,
-    League.NFL: 0.0
+    League.NFL: 0.05
 }
 
 # ALPHA controls how many predictions are needed to start getting rewards. Higher the ALPHA, the less predictions needed.
